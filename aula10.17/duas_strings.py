464d2427fca5d7d3b84c5660dc7b8876d206fddc
francisco oliveira <fs9457489@gmail.com>
2023-10-17 16:45:58 -0300

    atualizando comentarios no arquivo duas_strings.py

--- aula10.17/duas_strings.py
+++ aula10.17/duas_strings.py
@@ -1,4 +1,7 @@
-texto1 = "sou do" # testo1 recebe "sou do"
-texto2 = "back-end" # texto2 recebe "bank-end"
+# voce pode ESCAPAR das aspas duplas utilizado aspas simplis
+# vale reforcar que isso so vale para aspas duplas, outros caracteris nao
+texto1 = '"sou do"' # testo1 recebe "sou do"
+# caracterer de SCAPE e o \
+texto2 = "\"back-end\"" # texto2 recebe "bank-end"
 
 print(texto1, texto2) # print, conectar e imprime as duas variaveis
