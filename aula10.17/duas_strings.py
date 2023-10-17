3951849d9ee8ba05ca5b6dbd7caa87343c43bb4b
francisco oliveira <fs9457489@gmail.com>
2023-10-17 16:33:20 -0300

    adicionado comentario no arquivo duas_strings.py

--- aula10.17/duas_strings.py
+++ aula10.17/duas_strings.py
@@ -1,4 +1,4 @@
-texto1 = "sou do"
-texto2 = "back-end"
+texto1 = "sou do" # testo1 recebe "sou do"
+texto2 = "back-end" # texto2 recebe "bank-end"
 
-print(texto1, texto2)
+print(texto1, texto2) # print, conectar e imprime as duas variaveis
