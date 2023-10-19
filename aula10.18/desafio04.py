d8331d8e0ccf6021ad17a3f740daf0a01f7cb6f0
francisco oliveira <fs9457489@gmail.com>
2023-10-19 00:06:03 -0300

    corrigindo arquivo desafio04.py

--- aula10.18/desafio04.py
+++ aula10.18/desafio04.py
@@ -1,7 +1,6 @@
-nota1 = 6
-nota2 = 7
-nota3 = 5
-nota4 = 9
-nota = int(nota1) + int(nota2) + int(nota3) + int(nota4)
-media = nota /4
-print(media)
+nota1 = float(input("Digite sua 1 nota: "))
+nota2 = float(input("Digite sua 2 nota: "))
+nota3 = float(input("Digite sua 3 nota: "))
+nota4 = float(input("Digite sua 4 nota: "))
+media = (nota1 + nota2 + nota3 + nota4)/4
+print("Sua media é:",media)
