49f70f06732c8fde29e97c924018032200129b34
francisco oliveira <fs9457489@gmail.com>
2023-10-19 15:37:16 -0300

    comentario adicionado ao arquivo desafio03.py

--- aula10.18/desafio03.py
+++ aula10.18/desafio03.py
@@ -1,2 +1,4 @@
+# Faça um programa que peça um número e então mostre a mensagem "o número informado foi [número]"
+
 numero = input("Digite o numero que deseja: ")
 print("O numero informado foi",numero)
