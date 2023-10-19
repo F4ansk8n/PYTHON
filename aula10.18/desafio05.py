345c7776fce210988584b37cd67dd1990c7b6278
francisco oliveira <fs9457489@gmail.com>
2023-10-19 15:39:00 -0300

    comentario adicionado ao arquivo desafio05.py

--- aula10.18/desafio05.py
+++ aula10.18/desafio05.py
@@ -1,3 +1,5 @@
+# Faça um progranma que converta mentros para centimentros
+
 metro = float(input("imforme o valor em mentros: "))
 resultado = metro * 100
 print(metro,"equivale a",resultado, "cm")
