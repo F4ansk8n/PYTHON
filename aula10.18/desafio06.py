f3d4441d3dc798c62e9c5a24db26812315e4e404
francisco oliveira <fs9457489@gmail.com>
2023-10-19 15:39:36 -0300

    comentario adicionado ao arquivo desafio06.py

--- aula10.18/desafio06.py
+++ aula10.18/desafio06.py
@@ -1,3 +1,5 @@
+# Faça um programa que calcule a área de um quadrado, em seguinda mostre o dobro desta área para o usuario
+
 altura = float(input('Digite a altura do quadrado '))
 largura = float(input('Digite a largura do quadrado '))
 area = altura * largura
