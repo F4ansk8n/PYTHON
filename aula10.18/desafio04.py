7ba20e535067beacf4e41bf37a6c18e0e0f38590
francisco oliveira <fs9457489@gmail.com>
2023-10-19 15:38:31 -0300

    comentario adicionado ao arquivo desafio04.py

--- aula10.18/desafio04.py
+++ aula10.18/desafio04.py
@@ -1,3 +1,5 @@
+# Faça um programa que peça as 4 notas e mostre a média
+
 nota1 = float(input("Digite sua 1 nota: "))
 nota2 = float(input("Digite sua 2 nota: "))
 nota3 = float(input("Digite sua 3 nota: "))
