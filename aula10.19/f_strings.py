1b6de52826de5aa2e4e637f495ae2110715058b3
francisco oliveira <fs9457489@gmail.com>
2023-10-21 20:32:04 -0300

    criador o arquivo f_strings.py

---
+++ aula10.19/f_strings.py
@@ -0,0 +1,8 @@
+# Inteporlando strings com código python
+nome = input("Qual é seu nome? ")
+
+# Concatenação
+print("Seja muito bem-vindo", nome)
+
+# Interpolação
+print(f'Seja muito bem-vindo {nome}. Estamos esperado por você!!!')
