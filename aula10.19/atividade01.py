cd9fcbf9ba0c47c20a95ef116df982c2901346a1
francisco oliveira <fs9457489@gmail.com>
2023-10-22 18:24:38 -0300

    criador o arquivo da atividade01.py

---
+++ aula10.19/atividade01.py
@@ -0,0 +1,5 @@
+# Crie um código que peça a temperatura em graus Fahrenheit, transforme e imprima a temperatura em graus Celsius. Formula: C = 5 * ((F-32) / 9) 
+
+Fahrenheit = float(input("Informe a Temperatura em Fahrenheit: "))
+Celsius = (5*(Fahrenheit - 32) / 9)
+print("A temperatura em Celsius e: ",Celsius)
