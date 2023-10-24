514b9d966683f9c5c14a61b0c2a97349c2a880bf
francisco oliveira <fs9457489@gmail.com>
2023-10-24 01:47:38 -0300

    testando o arquivo da atividade02.py

--- aula10.19/atidade02.py
+++ aula10.19/atidade02.py
@@ -3,10 +3,12 @@
 # Para mulheres: (62.1*h) - 44.7 
 
 sexo = input('Digite seu sexo: (1)Para homem (2)Para mulher: ') 
-if 1:
-    altura_homem = float(input('Digite sua altura: '))
-    peso = (72.7 * altura_homem) - 58  
-else:
-    altura_mulher = float(input('Digite sua altura: ')) 
-    peso = (62.1 * altura_mulher)- 44.7 
-print('Seu peso ideal é ',peso,'quilos')
+if sexo == 1:
+     altura_homem = float(input('Digite sua altura: '))
+     peso = float(72.7 * altura_homem)
+     ideal = float (peso - 58)
+else :
+     altura_mulher = float(input('Digite sua altura: ')) 
+     peso = float(62.1 * altura_mulher) 
+     ideal = float(peso - 44.7)
+print ('Seu peso ideal é ',ideal, "quilos")
