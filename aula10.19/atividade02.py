ec5038723883c1b4fb7b003d84a845920289848e
francisco oliveira <fs9457489@gmail.com>
2023-10-26 14:34:36 -0300

    arquivo ainda com falha

--- aula10.19/atividade02.py
+++ aula10.19/atividade02.py
@@ -2,13 +2,15 @@
 # Para homens: (72.7*h) - 58
 # Para mulheres: (62.1*h) - 44.7 
 
-sexo = input('Digite seu sexo: (1)Para homem e (2)Para mulher: ') 
-if "1 para homem":
-     altura_homem = float(input('Digite sua altura: '))
-     peso = 72.7 * altura_homem
-     ideal = peso - 58
-else :
-     altura_mulher = float(input('Digite sua altura: ')) 
-     peso = 62.1 * altura_mulher
-     ideal = peso - 44.7
-print ('Seu peso ideal é ',ideal, "quilos")
+sexo1 = "Homem"
+sexo2 = "Mulher"
+input("informe seu sexo: ")
+if sexo1:
+     altura_homem = float(input("Informe sua altura: "))
+     peso = (72.7 * altura_homem) - 58
+     print ("Seu peso ideal é ",peso, "quilos")
+
+else:
+     altura_mulher = float(input("informe sua altura: ")) 
+     peso = (62.1 * altura_mulher) - 44.7
+     print ("Seu peso ideal é ",peso, "quilos")
