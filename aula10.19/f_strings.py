ca7cf4de55b688b221863589badb8e16b9aa7339
francisco oliveira <fs9457489@gmail.com>
2023-10-27 12:55:50 -0300

    aula10.19 delentada

--- aula10.19/f_strings.py
+++
@@ -1,8 +0,0 @@
-# Inteporlando strings com código python
-nome = input("Qual é seu nome? ")
-
-# Concatenação
-print("Seja muito bem-vindo", nome)
-
-# Interpolação
-print(f'Seja muito bem-vindo {nome}. Estamos esperado por você!!!')
