7d63439df54e3bf51be450e470d2cace4687add6
francisco oliveira <fs9457489@gmail.com>
2023-11-06 16:34:27 -0300

    aula  ambiente virtual

---
+++ aula11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,7 @@
+# variaveis são usadas para salvar algo na memória do computador.
+# os valores de uma variavel são mutavel.
+# o sunal de = e utilizado para atribuição de valores.
+var01 = "alguma coisa"
+print(var01)
+var01 = "outra coisa"
+print(var01)
