f1b7a0bb9288b1e2033bf71a662b6e157cc3bf81
francisco oliveira <fs9457489@gmail.com>
2023-11-22 15:27:35 -0300

    arquivos movidos para a pasta valley

---
+++ valley/aula11.21/atv_for_e_while.py
@@ -0,0 +1,37 @@
+# 1) aça um pograma que peça 10 numeros interos, calcule e mostre a quantidade de numeros pares e a quantidade de numeros impares.
+#contador_par = 0
+#contador_impar = 0
+
+#for i in range(1, 11):
+#    numero = int(input(f'Informeo o {i}° numero: '))
+#    if numero % 2 == 0:
+#        contador_par += 1
+#    else:
+#        contador_impar += 1
+
+#print(f'A quntidade de numeros pares é: {contador_par}')
+#print(f'A quntidade de numeros impares é: {contador_impar}')
+
+# 2) faça um progama que, dado um conjunto de N numeros determine o menor valor, o maior valor é a soma dos valores.
+#maior = 0
+#menor = None
+
+#while True:
+#    saida = input('Digite "S" para sair: ')
+#    if saida == 's' or saida == 'S':
+#        print('Volte sempre!')
+#        break
+
+#    numero = int(input('Informe um numero inteiro: '))
+
+#    if numero > maior:
+#        maior = numero
+
+#    if menor == None or numero < menor:
+#        menor = numero
+
+#print(f'Soma de {maior} + {menor} = {maior+menor}')
+#print(f'O maior valor é: {maior}')
+#print(f'O menor valor é: {menor}')
+
+# 3) 
