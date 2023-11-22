c809b4addf8b96f53f61a8622f23ddee254ab84e
francisco oliveira <fs9457489@gmail.com>
2023-11-22 16:37:13 -0300

    arquivo da avt de for e while

--- valley/aula11.21/atv_for_e_while.py
+++ valley/aula11.21/atv_for_e_while.py
@@ -1,4 +1,4 @@
-# 1) aça um pograma que peça 10 numeros interos, calcule e mostre a quantidade de numeros pares e a quantidade de numeros impares.
+# 1) faça um programa que peça 10 numeros interos, calcule e mostre a quantidade de numeros pares e a quantidade de numeros impares.
 #contador_par = 0
 #contador_impar = 0
 
@@ -12,7 +12,7 @@
 #print(f'A quntidade de numeros pares é: {contador_par}')
 #print(f'A quntidade de numeros impares é: {contador_impar}')
 
-# 2) faça um progama que, dado um conjunto de N numeros determine o menor valor, o maior valor é a soma dos valores.
+# 2) faça um programa que, dado um conjunto de N numeros determine o menor valor, o maior valor é a soma dos valores.
 #maior = 0
 #menor = None
 
@@ -34,4 +34,14 @@
 #print(f'O maior valor é: {maior}')
 #print(f'O menor valor é: {menor}')
 
-# 3) 
+# 3) faça um programa que peça um numero inteiro e determine se ele é ou não é primo. obs.: Um numero primo é aquele que é divisibel somente por ele mesmo e por 1.
+#numero = int(input('Informe um numero inteiro: '))
+#intervalo = range(1,numero + 1)
+#contador = 0
+
+#for i in intervalo:
+#    if numero % i == 0:
+#        print(f'foi divissivel por {i}')
+
+#if contador == 2 or contador == 1:
+#    print(f'O numero {numero} e primo')
