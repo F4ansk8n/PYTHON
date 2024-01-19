9504c7eb05980db8b64bed95bf0f40dd81c4e383
francisco oliveira <fs9457489@gmail.com>
2024-01-19 10:21:20 -0300

    quetoes

--- aula2023.12.20_(avaliacao)/questão_01.py
+++ aula2023.12.20_(avaliacao)/questão_01.py
@@ -11,3 +11,101 @@ Obs: você deve fazer o str de cada uma delas
 
 """
 
+class Veiculo:
+    def __init__(self, tipo, chassi, marca, modelo, ano):
+        self.tipo = tipo
+        self.chassi = chassi
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+
+    def get_tipo(self):
+        return self.tipo
+    
+    def get_chassi(self):
+        return self.chassi
+    
+    def get_marca(self):
+        return self.marca
+    
+    def get_modelo(self):
+        return self.modelo
+    
+    def get_anoo(self):
+        return self.ano
+
+    @property
+    def ano(self):
+        return self._ano
+
+    @_ano.setter
+    def _ano(self, valor: int):
+        self._ano = valor
+
+    
+class Carro(Veiculo):
+    def __init__(self, tipo, chassi, marca, modelo, ano, consumo, kilometragem, capacidade_tanque):
+        super().__init__(tipo, chassi, marca, modelo, ano)
+        self.__consumo = consumo                      # km/litro
+        self.__kilometragem = kilometragem            # Km atual do carro
+        self.__capacidade_tanque = capacidade_tanque  # definir quantidade de combustível que cabe no tanque
+        self.__combustivel_no_tanque = 0              # carro começa com tanque sem combustível
+
+    def __verifica_tanque(self, valor):
+        if ((self.__combustivel_no_tanque + valor) <= self.__capacidade_tanque): # verifica se o combustível vai caber no tanque
+            return valor
+        else:   #se não couber, vai completar o tanque
+            return (self.__capacidade_tanque - self.__combustivel_no_tanque) #retorna o que ainda cabe no tanque
+
+    def abastecer(self, volume):   # abastecer o carro com valor solicitado ou completar a capacidade
+        abastecer = self.__verifica_tanque(volume)
+        self.__combustivel_no_tanque += abastecer
+        print("Tanque abastecido com {} litros de combustível. Seu tanque agora tem {} litros".format(abastecer, self.__combustivel_no_tanque))
+
+    def __pode_andar(self, kilometragem):  # verifica se a quantidade de combustível no tanque é suficiente para rodar os Km solicitados
+        consumo = (kilometragem / self.__consumo) # litros que serão gastos na quilometragem percorrida
+        return (self.__capacidade_tanque >= consumo)
+
+    def andar(self, km): #Roda os km e adiciona no odometro do carro / reduz o combustível do tanque
+        if (self.__pode_andar(km)):
+            self.__kilometragem += km
+            self.__combustivel_no_tanque -= (km / self.__consumo)
+        else:
+            print("Combustível insuficiente para andar {}Km".format(km))
+
+    def tanque(self): #imprime a quantidade de litros que tem no tanque
+        print("Restam {} litros no tanque de combustível".format(self.__combustivel_no_tanque))
+
+    #propriedades da Classe Carro
+    @property
+    def consumo(self):
+        return self.__consumo
+
+    @property
+    def capacidade_tanque(self):
+        return self.__capacidade_tanque
+
+    @property
+    def kilometragem(self):
+        return self.__kilometragem
+
+    @property
+    def combustiven_no_tanque(self):
+        return self.__combustivel_no_tanque
+    
+
+class Motorbike(Veiculo):
+    def __init__(self, tipo, chassi, marca, modelo, ano,  potencia):
+        super().__init__(tipo, chassi, marca, modelo, ano)
+        self.potencia = potencia
+
+    def começar(self):
+        if self._verificar_gasolina:
+            print("Partida da moto!")
+        print("Sem gasolina")
+
+    def parar(self):
+        print("Parando a moto")
+
+    def _verificar_gasolina(self):
+        return False
--- aula2023.12.20_(avaliacao)/questão_04.py
+++ aula2023.12.20_(avaliacao)/questão_04.py
@@ -12,3 +12,60 @@ Obs: você deve ter um str mostrando o status completo do seu bichinho virtual
 -------------------------
 
 """
+
+print("\n\n\n CLASSE BICHINHO VIRTUAL \n\n\n")
+
+class BichinhoVirtual:
+	def __init__(self, nome, idade):
+		self.__nome = nome
+		self.__idade = idade
+		self.__fome = 100
+		self.__saude = 100
+
+	def get_nome(self):
+		return self.__nome
+
+	def set_nome(self, novo_nome):
+		self.__nome = novo_nome
+
+	def get_idade(self):
+		return self.__idade
+
+	def set_idade(self, nova_idade):
+		self.__idade = nova_idade
+
+	def get_fome(self):
+		return self.__fome
+
+	def set_fome(self, nova_fome):
+		self.__fome = nova_fome
+
+	def get_saude(self):
+		return self.__saude
+
+	def set_saude(self, nova_saude):
+		self.__saude = nova_saude
+
+	def get_humor(self):
+		if self.get_fome() >= 70 and self.get_saude() >= 70:
+			return "Estou feliz!"
+
+		elif self.get_fome() >= 50 and self.get_saude() >= 50:
+			return "Não sou tão bom!"
+
+		elif self.get_fome() >= 30 and self.get_saude() >= 30:
+			return "Estou com muita raiva!"
+
+		else:
+			return "Estou tão fraco que não consigo responder!"
+
+bichinho = BichinhoVirtual("Bolinha", 2)
+
+print( bichinho.get_nome() )
+print( bichinho.get_idade() )
+print( bichinho.get_humor() )
+
+bichinho.set_saude(30)
+bichinho.set_fome(70)
+
+print( bichinho.get_humor() )
